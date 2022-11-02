3917ff5e2a45268b3e2276bb44b069838f5ddea2
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-11-01 22:15:50 -0300

    Last commit in the day 1/11

--- client/src/Components/ActivityCreate/ActivityCreate.jsx
+++ client/src/Components/ActivityCreate/ActivityCreate.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getActivities, postActivities } from "../../Redux/actions";
 
 const ActivityCreate = () => {
+  
+  const dispatch = useDispatch();
+  // const history = useHistory();
+  const countries = useSelector((state) => state.allCountries);
+
+  useEffect(() => {
+    dispatch(getActivities());
+  }, [dispatch]);
+
   return (
-    <div>ActivityCreate</div>
+    <div>
+      ActivityCreate  
+      </div>
   )
 }
 
