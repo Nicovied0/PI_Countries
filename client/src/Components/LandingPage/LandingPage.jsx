40c08779d7a709ebf48b62e3059f519478a1b7a2
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-11-03 11:51:36 -0300

    styles button landing page

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
   return (
     <div className={style.container}>
       <Link to={'/home'}>
-        <button>Home</button>
+        <button className={style.button_home}>Home</button>
       </Link>
     </div> 
   )
