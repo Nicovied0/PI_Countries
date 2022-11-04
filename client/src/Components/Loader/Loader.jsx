1ea25567fd22eb8a74cefdb5458d9cc7fbb621e0
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-11-04 18:05:39 -0300

    Loader ready just need to apply in to states

---
+++ client/src/Components/Loader/Loader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from './Loader.module.css'
+
+const Loader = () => {
+  return (
+    <div>
+      <div class={style.ldsRoller}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Loader;
