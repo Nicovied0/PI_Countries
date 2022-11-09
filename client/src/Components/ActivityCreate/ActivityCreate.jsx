a253d519cbb0570e6df59e45ac59dcedc11ad920
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-11-09 20:00:11 -0300

    input name cotroled

--- client/src/Components/ActivityCreate/ActivityCreate.jsx
+++ client/src/Components/ActivityCreate/ActivityCreate.jsx
@@ -4,13 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getActivities, postActivities } from "../../Redux/actions";
 import { SUMMER, FALL, WINTER, SPRING } from "../../Const/Const";
 import NavBar from "../Nav_bar/NavBar";
-import style from './ActivityCreate.module.css'
+import style from "./ActivityCreate.module.css";
 
 function validate(input) {
+  console.log(input.name.trim())
   //function with verificated to errors in the form
   let errors = {};
-  if (!input.name) {
-    errors.name = "You must complete this field";
+  if (!input.name || !input.name.trim().replace(/\s+/g, ' ') || !/^[a-zA-Z áéíóúÁÉÍÓÚñÑ\s]*$/.test(input.name) || input.name.length < 3) {
+    errors.name = "Name of activity must contain only letters"; //.trim para quitar espacios vacios al principio y al final, .remplace para quitar espacios en blanco y expresion regular testeada con el .test para verificar que sea alfanumerico y 
   } else if (!input.duration) {
     errors.duration = "You must complete this field";
   } else if (!input.difficulty) {
@@ -27,6 +28,9 @@ const ActivityCreate = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const countries = useSelector((state) => state.allCountries);
+
+  // const [error, setError] = useState("");
+  // const [mostrarError, setMostrarError] = useState(false);
   const [errors, setErrors] = useState({});
 
   const [input, setInput] = useState({
@@ -38,7 +42,8 @@ const ActivityCreate = () => {
   });
 
   function handleChange(e) {
-    if(input.name)
+    // console.log(input.name.trim());
+
     setInput({
       ...input,
       [e.target.name]: e.target.value,
@@ -59,11 +64,13 @@ const ActivityCreate = () => {
   }
 
   function handleSelect(e) {
-    if(!input.countryId.includes(e.target.value))
-    setInput({
-      ...input,
-      countryId: [...input.countryId, e.target.value ],
-    });
+    if (!input.countryId.includes(e.target.value))
+      //If input === a un input ya ingresado no se ingresa
+      setInput({
+        ...input,
+        countryId: [...input.countryId, e.target.value],
+      });
+    console.log(input);
   }
 
   useEffect(() => {
@@ -94,7 +101,7 @@ const ActivityCreate = () => {
   }
 
   return (
-    <div >
+    <div>
       <div>
         <NavBar />
         <div className={style.container}>
@@ -115,113 +122,81 @@ const ActivityCreate = () => {
                 />
                 {errors.name && <p className={style.error}>{errors.name}</p>}
               </div>
-            
-              <div  className={style.divInput}>
+
+              <div className={style.divInput}>
                 <label>Activity duration in minutes</label>
                 <select
                   value={input.duration}
                   name="duration"
                   onChange={handleChange}
                 >
-                  <option value={"30"}>
-                    30
-                  </option>
-                  <option value={"60"}>
-                    60
-                  </option>
-                  <option value={"90"}>
-                    90
-                  </option>
-                  <option value={"120"}>
-                    120
-                  </option>
-                  <option value={"150"}>
-                    150
-                  </option>
-                  <option value={"180"}>
-                    180
-                  </option>
-                  <option value={"210"}>
-                    210
-                  </option>
-                  <option value={"240"}>
-                    240
-                  </option>
+                  <option value={"30"}>30</option>
+                  <option value={"60"}>60</option>
+                  <option value={"90"}>90</option>
+                  <option value={"120"}>120</option>
+                  <option value={"150"}>150</option>
+                  <option value={"180"}>180</option>
+                  <option value={"210"}>210</option>
+                  <option value={"240"}>240</option>
                 </select>
-                {errors.duration && <p className={style.error}>{errors.duration}</p>}
+                {errors.duration && (
+                  <p className={style.error}>{errors.duration}</p>
+                )}
               </div>
 
-              <div  className={style.divInput}>
+              <div className={style.divInput}>
                 <label> Difficulty </label>
                 <select
                   name="difficulty"
                   value={input.difficulty}
                   onChange={(e) => handleChange(e)}
                 >
-                  <option value={"1"}>
-                    1
-                  </option>
-                  <option value={"2"}>
-                    2
-                  </option>
-                  <option value={"3"}>
-                    3
-                  </option>
-                  <option value={"4"}>
-                    4
-                  </option>
-                  <option value={"5"}>
-                    5
-                  </option>
+                  <option value={"1"}>1</option>
+                  <option value={"2"}>2</option>
+                  <option value={"3"}>3</option>
+                  <option value={"4"}>4</option>
+                  <option value={"5"}>5</option>
                 </select>
-                {errors.difficulty && <p className={style.error}> {errors.difficulty}</p>}
+                {errors.difficulty && (
+                  <p className={style.error}> {errors.difficulty}</p>
+                )}
               </div>
 
-
-              <div  className={style.divInput}>
+              <div className={style.divInput}>
                 <label>Season</label>
                 <select
                   name="season"
                   value={input.season}
                   onChange={(e) => handleChange(e)}
                 >
-                  <option value={WINTER}>
-                    Winter
-                  </option>
-                  <option value={SUMMER}>
-                    Summer
-                  </option>
-                  <option value={FALL}>
-                    Fall
-                  </option>
-                  <option value={SPRING}>
-                    Spring
-                  </option>
+                  <option value={WINTER}>Winter</option>
+                  <option value={SUMMER}>Summer</option>
+                  <option value={FALL}>Fall</option>
+                  <option value={SPRING}>Spring</option>
                 </select>
-                {errors.season && <p className={style.error}>{errors.season}</p>}
+                {errors.season && (
+                  <p className={style.error}>{errors.season}</p>
+                )}
               </div>
-              {errors.countryId && <p className={style.error}>{errors.countryId}</p>}
+              {errors.countryId && (
+                <p className={style.error}>{errors.countryId}</p>
+              )}
 
-
-              <div  className={style.divInput}>
+              <div className={style.divInput}>
                 <label>Enter country</label>
                 <select onChange={(e) => handleSelect(e)}>
-                  <option > Countries </option>
+                  <option> Countries </option>
                   {countries.map((e) => (
-                    <option value={e.id}>
-                      {e.name}
-                    </option>
+                    <option value={e.id}>{e.name}</option>
                   ))}
                 </select>
               </div>
-
-
 
               <div className={style.divCountrie}>
                 {input.countryId.map((country) => (
                   <div className={style.divcountryId}>
                     <input
-                      className={style.inputButton }
+                      className={style.inputButton}
                       type="button"
                       value="X"
                       onClick={() => handleDelete(country)}
@@ -232,7 +207,7 @@ const ActivityCreate = () => {
               </div>
 
               <div>
-                <button className={style.inputButton } type="submit">
+                <button className={style.inputButton} type="submit">
                   Create Activity
                 </button>
               </div>
